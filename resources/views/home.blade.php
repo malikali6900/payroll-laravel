7d19510d3312db162efa388c81f0f18208768e9a
Ali Ahmed <malikali6900@gmail.com>
2023-09-30 14:52:49 +0500

    Graphical representation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -592,7 +592,7 @@
           <div class="section-heading">
             <h6>Contact Us</h6>
             <h2>Feel free to contact us anytime</h2>
-            <p>Thank you for choosing our templates. We provide you best CSS templates at absolutely 100% free of charge. You may support us by sharing our website to your friends.</p>
+            {{-- <p>Thank you for choosing our templates. We provide you best CSS templates at absolutely 100% free of charge. You may support us by sharing our website to your friends.</p> --}}
             <div class="special-offer">
               <span class="offer">off<br><em>50%</em></span>
               <h6>Valide: <em>24 April 2036</em></h6>
