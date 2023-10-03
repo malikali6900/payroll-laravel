866d1da3422d0b34666cdea4c93be3faeeae173a
Ali Ahmed <malikali6900@gmail.com>
2023-10-03 12:53:37 +0500

    content changing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -585,14 +585,13 @@
     </div>
   </div>
 
-  <div class="contact-us section" id="contact">
+  {{-- <div class="contact-us section" id="contact">
     <div class="container">
       <div class="row">
         <div class="col-lg-6  align-self-center">
           <div class="section-heading">
             <h6>Contact Us</h6>
             <h2>Feel free to contact us anytime</h2>
-            {{-- <p>Thank you for choosing our templates. We provide you best CSS templates at absolutely 100% free of charge. You may support us by sharing our website to your friends.</p> --}}
             <div class="special-offer">
               <span class="offer">off<br><em>50%</em></span>
               <h6>Valide: <em>24 April 2036</em></h6>
@@ -631,7 +630,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
 
   <footer>
     <div class="container">
